6a0d73a6e38bb64553b99130e7c38d0d955eb8fb
JKjiwon <jwkim.oa@gmail.com>
2023-08-26 18:05:51 +0900

    init project

---
+++ src/main/kotlin/com/lgtm/simplesns/SimpleSnsApplication.kt
@@ -0,0 +1,11 @@
+package com.lgtm.simplesns
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleSnsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SimpleSnsApplication>(*args)
+}
---
+++ src/test/kotlin/com/lgtm/simplesns/SimpleSnsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lgtm.simplesns
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleSnsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
