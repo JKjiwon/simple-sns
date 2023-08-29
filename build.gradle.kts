7da554d0bc6a6fc6877ac2d14199edb6ae0eeac7
JKjiwon <jwkim.oa@gmail.com>
2023-08-29 15:04:20 +0900

    build: spring-security, jwt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,16 @@ repositories {
 }
 
 dependencies {
+    // swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+    // spring security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // jwt
+    implementation ("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
