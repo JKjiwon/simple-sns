b65fa80f23d612d1a413760f2a2c5a46f9b537ac
JKjiwon <jwkim.oa@gmail.com>
2023-09-07 18:46:57 +0900

    refactor: AuthenticationProvider의 빈을 수동으로 등록

--- src/main/kotlin/com/lgtm/simplesns/application/usecase/SignInMemberUsecase.kt
+++ src/main/kotlin/com/lgtm/simplesns/application/usecase/SignInMemberUsecase.kt
@@ -4,20 +4,20 @@ import com.lgtm.simplesns.application.dto.MemberSignInCommand
 import com.lgtm.simplesns.security.jwt.JwtToken
 import com.lgtm.simplesns.security.jwt.JwtUtils
 import com.lgtm.simplesns.security.userdetail.MemberDetails
+import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 
 @Service
 class SignInMemberUsecase(
     private val jwtUtils: JwtUtils,
-    private val authenticationManagerBuilder: AuthenticationManagerBuilder
+    private val authenticationManager: AuthenticationManager
 ) {
 
     fun execute(command: MemberSignInCommand): JwtToken {
         val usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken(command.email, command.password)
-        val authentication = authenticationManagerBuilder.`object`.authenticate(usernamePasswordAuthenticationToken)
+        val authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken)
         SecurityContextHolder.getContext().authentication = authentication
 
         val principal = authentication.principal as MemberDetails
--- src/main/kotlin/com/lgtm/simplesns/security/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/lgtm/simplesns/security/config/WebSecurityConfig.kt
@@ -1,20 +1,15 @@
 package com.lgtm.simplesns.security.config
 
 import com.lgtm.simplesns.security.filter.JwtAuthenticationFilter
-import com.lgtm.simplesns.security.provider.JwtAuthenticationProvider
-import com.lgtm.simplesns.security.userdetail.MemberDetailService
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.ProviderManager
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher
@@ -29,7 +24,7 @@ private val SWAGGER_PATH_PATTERNS = arrayOf(
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 class WebSecurityConfig(
-    private val jwtAuthenticationProvider: JwtAuthenticationProvider,
+    private val authenticationManager: AuthenticationManager
 ) {
 
     @Bean
@@ -57,15 +52,7 @@ class WebSecurityConfig(
 
     @Bean
     fun jwtAuthenticationFilter(): JwtAuthenticationFilter {
-        val authenticationManager = ProviderManager(jwtAuthenticationProvider)
         return JwtAuthenticationFilter(authenticationManager)
-    }
-
-    @Bean
-    fun daoAuthenticationProvider(userDetailService: MemberDetailService): DaoAuthenticationProvider {
-        val daoAuthenticationProvider = DaoAuthenticationProvider(passwordEncoder())
-        daoAuthenticationProvider.setUserDetailsService(userDetailService)
-        return daoAuthenticationProvider
     }
 
     @Bean
@@ -76,10 +63,5 @@ class WebSecurityConfig(
                 .requestMatchers(PathRequest.toH2Console())
                 .requestMatchers(*swaggerPathMatcher)
         }
-    }
-
-    @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
     }
 }
