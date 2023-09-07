8ab79df1ec943139a2fc0ddee42a99d4f5431fab
JKjiwon <jwkim.oa@gmail.com>
2023-09-07 20:51:12 +0900

    refactor: applicationException 적용

--- src/main/kotlin/com/lgtm/simplesns/domain/follow/entity/Follow.kt
+++ src/main/kotlin/com/lgtm/simplesns/domain/follow/entity/Follow.kt
@@ -5,21 +5,10 @@ import jakarta.persistence.*
 import org.springframework.util.Assert
 
 @Entity
-@Table(
-    name = "follow",
-    uniqueConstraints = [
-        UniqueConstraint(name = "uk_follow", columnNames = ["fromMemberId", "toMemberId"])
-    ]
-)
-
 class Follow(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
     val fromMemberId: Long,
     val toMemberId: Long,
-) : BaseEntity() {
-    init {
-        Assert.isTrue(fromMemberId != toMemberId, "From, To 회원이 동일합니다.")
-    }
-}
+) : BaseEntity()
--- src/main/kotlin/com/lgtm/simplesns/domain/follow/service/FollowWriteService.kt
+++ src/main/kotlin/com/lgtm/simplesns/domain/follow/service/FollowWriteService.kt
@@ -1,5 +1,7 @@
 package com.lgtm.simplesns.domain.follow.service
 
+import com.lgtm.simplesns.application.common.ApplicationException
+import com.lgtm.simplesns.application.common.ErrorCode
 import com.lgtm.simplesns.domain.follow.entity.Follow
 import com.lgtm.simplesns.domain.follow.repository.FollowRepository
 import com.lgtm.simplesns.domain.member.dto.MemberDto
@@ -14,6 +16,11 @@ class FollowWriteService(
 
     fun create(fromMember: MemberDto, toMember: MemberDto) {
         val follow = Follow(fromMemberId = fromMember.id, toMemberId = toMember.id)
+
+        if (follow.fromMemberId == follow.toMemberId) {
+            throw ApplicationException(ErrorCode.EQUAL_FOLLOWER_FOLLOWING)
+        }
+
         followRepository.save(follow)
     }
 
